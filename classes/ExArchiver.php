aed4c532c16db3bb12236c31a34ad82c51dca40b
Oscar Berry <oscar@madokami.com>
2014-08-14 00:21:12 +0100

    Support for expunged param in feeds

--- classes/ExArchiver.php
+++ classes/ExArchiver.php
@@ -47,7 +47,13 @@ class ExArchiver {
 			while(true) {
 				Log::debug(self::LOG_TAG, 'Crawling feed "%s", page %d', $feed->term, $page);
 
-				$indexHtml = $this->client->index($feed->term, $page);
+				$params = array();
+
+				if($feed->expunged) {
+					$params['f_sh'] = 1;
+				}
+
+				$indexHtml = $this->client->index($feed->term, $page, $params);
 				$indexPage = new ExPage_Index($indexHtml);
 				
                 $isLastPage = $indexPage->isLastPage();
