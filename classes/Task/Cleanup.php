6ecdd9ca2e406c88aa750a3160604420f0d1444d
Kyle Donaldson <snowcrack123@gmail.com>
2015-08-11 22:12:20 +1000

    Still don't use the Cleanup function. I've added thumbnail deletion now, removes both gallery_thumb and image references. As well as deleting the gallery itself, so don't run it as it will leave things left over and won't let you do anything else.

--- classes/Task/Cleanup.php
+++ classes/Task/Cleanup.php
@@ -5,6 +5,7 @@ class Task_Cleanup extends Task_Abstract {
 	const LOG_TAG = 'Task_Cleanup';
 	
 	public function run($options = array()) {
+		$config = Config::get();
 		Log::debug(self::LOG_TAG, 'Checking for NULL galleryproperty entries');
 		
 		$entries = R::getAll('SELECT * FROM galleryproperty WHERE gallery_id IS NULL');
@@ -20,6 +21,38 @@ class Task_Cleanup extends Task_Abstract {
 		}
 		else {
 			Log::debug(self::LOG_TAG, 'No NULL galleryproperty entries found.');
+		}
+		
+		Log::debug(self::LOG_TAG, 'Checking for any galleries marked as deleted.');
+		$entries = R::getAll('SELECT * FROM gallery WHERE deleted = 1');
+		if (count($entries) > 0) {
+			$count = 0;
+			Log::debug(Self::LOG_TAG, 'Deleting galleries now. This may take a while.');
+			foreach ($entries as $entry) {
+				$gallery = R::load('gallery', $entry["id"]);
+				$galleryproperty = R::getAll('SELECT * FROM galleryproperty WHERE gallery_id = ?', array($entry["id"]));
+				$gallery_tag = R::getAll('SELECT * FROM gallery_tag WHERE gallery_id = ?', array($entry["id"));
+				$gallery_thumb = R::getAll('SELECT * FROM gallery_thumb WHERE gallery_id = ?', array($entry["id"));
+				
+				// Get all image entries associated with the gallery_thumb then delete those, then delete the gallery_thumb entry
+				foreach ($gallery_thumb as $thumb) {
+					$images = R::getAll('SELECT * FROM image WHERE id = ?', array($thumb["image_id"]));
+					foreach ($images as $image) {
+						$book = R::load('image', $image["id"]);
+						$bookImage = Model_Image::loadBean($book);
+						$bookImage::delete();
+						R::trash($book);
+					}
+					$book = R::load('gallery_thumb', $thumb["id"]);
+					R::trash($book);
+				}
+				R::trash($gallery);
+				$count++
+			}
+			Log::debug(self::LOG_TAG, 'All galleries marked as deleted were deleted. Total deleted was %d', $count);
+		}
+		else {
+			Log::debug(self::LOG_TAG, 'No galleries found that were marked as deleted.');
 		}
 	}
 	
