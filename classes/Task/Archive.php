76c1ea1c76f4bfd402312cfc8b6dd1c4b8bf58f0
Sn0wCrack <snowcrack123@gmail.com>
2017-07-29 10:35:29 +1000

    Code cleanup

--- classes/Task/Archive.php
+++ classes/Task/Archive.php
@@ -5,7 +5,9 @@ class Task_Archive extends Task_Abstract {
 	public function run($options = array()) {
 		$archiver = new ExArchiver();
 		if (isset($options[0])) {
-			if ($options[0] == "Feed") {
+			$opt = strtolower($options[0]);
+
+			if ($opt == "feed") {
 				$archiver->start((int)$options[1]);
 			} else {
 				$archiver->start();
