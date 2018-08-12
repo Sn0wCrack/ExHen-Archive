6e7aaf9c7f74f700ed7839f7bc1885785dbb448f
Oguzhan Uysal <development@oguzhanuysal.eu>
2018-08-12 03:22:10 +0200

    Add CORS headers to nginx Add www/config.json to .gitignore Add EXIF extension to Dockerfile Replace removed read_exif_data with exif_read_data (former aliases later)

--- Dockerfile
+++ Dockerfile
@@ -32,6 +32,7 @@ RUN apk add --no-cache --update curl-dev jpeg-dev freetype-dev mysql-client \
     && docker-php-ext-install -j$(nproc) pdo_mysql \
     && docker-php-ext-install -j$(nproc) curl \
     && docker-php-ext-install -j$(nproc) zip \
+    && docker-php-ext-install -j$(nproc) exif \
     && docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/ \
     && docker-php-ext-install -j$(nproc) gd
 
--- docker-entrypoint-init.d/10-config.sh
+++ docker-entrypoint-init.d/10-config.sh
@@ -2,6 +2,7 @@
 if [ -f "config.json.linux" ]; then
 echo "Updating config.json"
 
-jq '.base.cookie.ipb_member_id=env.CONF_MEMBERID | .base.cookie.ipb_pass_hash=env.CONF_PASSHASH | .base.cookie.ipb_accesskey=env.CONF_ACCESSKEY | .base.tempDir=env.CONF_TEMPDIR | .base.archiveDir=env.CONF_ARCHDIR | .base.imageDir=env.CONF_IMGDIR | .default.db.dsn=env.CONF_SQLDSN | .default.db.user=env.DB_USER | .default.db.pass=env.DB_PASS | .default.sphinxql.dsn=env.CONF_SPHINXDSN | .default.memcache.host="memcache"' config.json.linux > config.json
+jq '.base.cookie.ipb_member_id=env.CONF_MEMBERID | .base.cookie.ipb_pass_hash=env.CONF_PASSHASH | .base.cookie.ipb_accesskey=env.CONF_ACCESSKEY | .base.tempDir=env.CONF_TEMPDIR | .base.archiveDir=env.CONF_ARCHDIR | .base.imagesDir=env.CONF_IMGDIR | .default.db.dsn=env.CONF_SQLDSN | .default.db.user=env.DB_USER | .default.db.pass=env.DB_PASS | .default.sphinxql.dsn=env.CONF_SPHINXDSN | .default.memcache.host="memcache"' config.json.linux > config.json
+cp config.json.linux www/config.json
 echo "Updated config"
 fi
